27e5e5bc7075f826f3b6645c8e766c5ceb349f78
Cody Brocious <cody.brocious@gmail.com>
2013-12-11 20:11:17 -0500

    Added memcpy

---
+++ functions/memcpy.asm
@@ -0,0 +1,16 @@
+#$ memcpy A(2Ah)
+
+# memcpy(src, dest, n)
+export_memcpy:
+	beq $0, $a2, Lret
+
+	Lloop:
+		lbu $t0, 0($a0)
+		sb $t0, 0($a1)
+		addi $a0, $a0, 1
+		addi $a1, $a1, 1
+		addi $a2, $a2, -1
+		bne $0, $a2, Lloop
+
+	Lret:
+		jr $ra
